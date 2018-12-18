06cb34c231a00bb0b0b6900d55aa0927bf78a14a
martinebel <36831742+martinebel@users.noreply.github.com>
2018-12-18 14:18:17 -0300

    Agregado mensaje de datos personales

--- register.php
+++ register.php
@@ -10,7 +10,7 @@
 			$telefono=$_POST['telefono'];
 			$localidad=$_POST['localidad'];
 			$provincia=$_POST['provincia'];
-			
+
 	  $stmt = $dbh->prepare("insert into clientes values(NULL,'".$nombre."','".$cuit."','".$telefono."','".$direccion."','".$localidad."','".$provincia."','".$email."','".$password."',1,NULL)");
 	   $stmt->execute();
 	    echo '<div class="alert alert-success alert-dismissible" role="alert">
@@ -18,12 +18,12 @@
   Ha sido registrado correctamente! Puede iniciar su sesion <a href="login.php">haciendo clic aqui</a>
 </div>';
   }*/
- 
+
   ?>
     <div class="container" style=" padding-top: 20px;">
-	
+
 <br>
-			
+
 	<div class="panel panel-default">
   <div class="panel-heading">
     <h3 class="panel-title">Registrarse</h3>
@@ -31,7 +31,7 @@
   <div class="panel-body">
      <form id="commentsForm" method="post" style="padding:10px;">
                             <fieldset>
-						
+
 							<input type="hidden" name="action" value="misdatos">
 							<div class="row">
 							<div class="col-md-12">
@@ -46,7 +46,7 @@
     <input type="text" class="form-control" id="cuit" name="cuit"  placeholder="DNI / CUIT" required>
   </div>
   </div>
-  
+
    <div class="col-md-4">
                                  <div class="form-group">
     <label for="telefono">Tipo Cliente</label>
@@ -58,7 +58,7 @@
 	</select>
   </div>
   </div>
- 
+
    <div class="col-md-4">
                                  <div class="form-group">
     <label for="telefono">Telefono</label>
@@ -95,31 +95,33 @@
     <input type="password" class="form-control" id="pass" name="pass"  required>
   </div>
   </div>
-                                
+
                               <div class="col-md-6 col-md-offset-3">
                                 <input type="submit" id="botonRegistro" class="btn btn-lg btn-success btn-block" value="Registrarme">
+                                <br>
+                                <div class="alert alert-info" role="alert"><i class="fa fa-info-circle"></i>  Sus datos personales seran utilizados para identificacion y no seran compartidos con terceros. No se utilizara su telefono, direccion ni casilla de email para el envio de publicidad.</div>
 								</div>
                             </fieldset>
                         </form>
   </div>
-</div>			
-					
+</div>
+
 				</div>
-			
+
       <?php
   include 'footer.php';
   ?>
-  
+
   <script>
   $(document).on("input", "#cuit", function() {
     this.value = this.value.replace(/\D/g,'');
 });
 
   $("#commentsForm").submit(function(event){
-	  
+
 	   event.preventDefault();
 	    $("#botonRegistro").attr('disabled', 'disabled');
-	   $(".alert").remove();
+	   $("#alerta").remove();
     var form=$("#commentsForm");
     $.ajax({
         type: "POST",
@@ -129,23 +131,23 @@
     .success(function(data, textStatus, jqXHR){
 
       if(data.code=="201"){
-        $("#commentsForm").prepend('<div class="alert alert-success alert-dismissible" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>Ha sido registrado correctamente! Puede iniciar su sesion <a href="login.php">haciendo clic aqui</a></div>');
-      
+        $("#commentsForm").prepend('<div id="alerta" class="alert alert-success alert-dismissible" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>Ha sido registrado correctamente! Puede iniciar su sesion <a href="login.php">haciendo clic aqui</a></div>');
+
 		$('#commentsForm')[0].reset();
 		$("#botonRegistro").removeAttr('disabled');
   }
   else
   {
 
-     $("#commentsForm").prepend('<div class="alert alert-danger alert-dismissible" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>Ya existe un usuario registrado con este numero de CUIT.</div>');
+     $("#commentsForm").prepend('<div id="alerta"  class="alert alert-danger alert-dismissible" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>Ya existe un usuario registrado con este numero de CUIT.</div>');
         $("#botonRegistro").removeAttr('disabled');
   }
     })
     .fail(function(jqXHR, textStatus, errorThrown){
-       
+      $("#commentsForm").prepend('<div id="alerta"  class="alert alert-danger alert-dismissible" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>Ya existe un usuario registrado con este numero de CUIT.</div>');
+         $("#botonRegistro").removeAttr('disabled');
     });
     event.preventDefault();
 	return false;
 });
   </script>
-  
