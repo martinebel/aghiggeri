001b835cd2ecc3c07b81a7e918da01af3b7ce252
martinebel <36831742+martinebel@users.noreply.github.com>
2018-12-18 14:15:57 -0300

    Blanqueo de Clave

--- admin/clientes.php
+++ admin/clientes.php
@@ -1,9 +1,9 @@
-<?php 
+<?php
 require '../db.php';
 if(isset($_POST['action']))
 {
-	
-	
+
+
 	 $codigo= $_POST['id'];
 	 $idsistema= $_POST['idsistema'];
 	 $razonsocial= $_POST['razonsocial'];
@@ -16,9 +16,9 @@ if(isset($_POST['action']))
 	 $tipo= $_POST['tipo'];
         $stmt = $dbh->prepare("update clientes set razonsocial='".$razonsocial."', cuit='".$cuit."',telefono='".$telefono."',direccion='".$direccion."',localidad='".$localidad."',provincia='".$provincia."',email='".$email."',tipo='".$tipo."',idsistema='".$idsistema."' where idcliente=".$codigo);
 $stmt->execute();
-   
+
 echo '<div class="alert alert-success" role="alert">CLIENTE MODIFICADO CON EXITO</div>';
-		
+
 
 }
 if(isset($_REQUEST['action']))
@@ -26,8 +26,15 @@ if(isset($_REQUEST['action']))
 	if($_REQUEST["action"]=="del"){
 $stmt = $dbh->prepare("delete from clientes where idcliente=".$_REQUEST['id']);
 $stmt->execute();
-   
-echo '<div class="alert alert-warning" role="alert">CLIENTE ELIMINADO CON EXITO</div>';
+
+echo '<div class="alert alert-danger" role="alert">CLIENTE ELIMINADO CON EXITO</div>';
+}
+
+if($_REQUEST["action"]=="clave"){
+$stmt = $dbh->prepare("update clientes set password='123' where idcliente=".$_REQUEST['id']);
+$stmt->execute();
+
+echo '<div class="alert alert-warning" role="alert">CLAVE RESETEADA A 123</div>';
 }
 }
 include 'pages/clientes.tpl.php';
--- admin/pages/editcliente.tpl.php
+++ admin/pages/editcliente.tpl.php
@@ -6,7 +6,7 @@
                     <h1 class="page-header">Editar Cliente</h1>
                 </div>
             </div>
-           
+
              <div class="row">
                 <div class="col-lg-12">
 				 <div class="panel-body">
@@ -16,7 +16,7 @@
 				 <?php
  $stmt = $dbh->prepare("SELECT * from clientes where idcliente=".$_REQUEST['id']);
         $stmt->execute();
-		$result = $stmt->fetchAll(); 
+		$result = $stmt->fetchAll();
 		foreach($result as $row)
 		{
 			$id=$row['idcliente'];
@@ -31,46 +31,65 @@
 			$tipo=$row['tipo'];
 		}
  ?>
-  <div class="form-group">
-  <label for="idsistema">Codigo Sistema:</label>
-  <input type="text" class="form-control" id="idsistema" name="idsistema" value="<?php echo $idsistema;?>">
-</div>
 
+
+<div class="col-md-12">
  <div class="form-group">
   <label for="usr">Razon Social:</label>
   <input type="text" class="form-control" id="usr" name="razonsocial" value="<?php echo $razonsocial;?>" required>
 </div>
+</div>
 
+<div class="col-md-4">
  <div class="form-group">
   <label for="marca">CUIT / CUIL / DNI:</label>
   <input type="text" class="form-control" id="marca" name="cuit" value="<?php echo $cuit;?>" required>
 </div>
+</div>
 
+<div class="col-md-4">
  <div class="form-group">
   <label for="telefono">Telefono:</label>
    <input type="text" class="form-control" id="telefono" name="telefono" value="<?php echo $telefono;?>" required>
 </div>
+</div>
 
+<div class="col-md-4">
+ <div class="form-group">
+ <label for="idsistema">Codigo Sistema:</label>
+ <input type="text" class="form-control" id="idsistema" name="idsistema" value="<?php echo $idsistema;?>">
+</div>
+</div>
+
+<div class="col-md-4">
  <div class="form-group">
   <label for="direccion">Direccion:</label>
   <input type="text" class="form-control" id="direccion" name="direccion" value="<?php echo $direccion;?>" required>
 </div>
+</div>
 
+<div class="col-md-4">
  <div class="form-group">
   <label for="localidad">Localidad:</label>
   <input type="text" class="form-control" id="localidad" name="localidad" value="<?php echo $localidad;?>" required>
 </div>
+</div>
 
+<div class="col-md-4">
  <div class="form-group">
   <label for="provincia">Provincia:</label>
   <input type="text" class="form-control" id="provincia" name="provincia" value="<?php echo $provincia;?>" required>
 </div>
+</div>
 
+<div class="col-md-6">
  <div class="form-group">
   <label for="email">Email:</label>
   <input type="text" class="form-control" id="email" name="email" value="<?php echo $email;?>" required>
 </div>
+</div>
 
+<div class="col-md-6">
  <div class="form-group">
   <label for="tipo">Tipo:</label>
   <select class="form-control" id="tipo" name="tipo" required>
@@ -80,17 +99,18 @@
   <option value="3" <?php if($tipo=="3"){echo " selected ";}?>>Repuestero</option>
   </select>
 </div>
- 
+</div>
 
 <input type="submit" value="Guardar" class="btn btn-success">
+<a href="clientes.php?action=clave&id=<?php echo $_REQUEST['id'];?>" class="btn btn-warning">Blanquear Clave</a>
 <a href="clientes.php?action=del&id=<?php echo $_REQUEST['id'];?>" class="btn btn-danger pull-right">ELIMINAR</a>
 				 </form>
-                   
+
 				</div>
 				</div>
-           
+
             </div>
-           
+
         </div>
         <!-- /#page-wrapper -->
 
